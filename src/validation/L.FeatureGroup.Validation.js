aa581d1d7f7de38e06786ad503d4c5b2a9d47714
rthoth <ronaldo.silva@lemaf.ufla.br>
2015-10-07 20:42:52 -0300

    Comming...

--- src/validation/L.FeatureGroup.Validation.js
+++ src/validation/L.FeatureGroup.Validation.js
@@ -17,8 +17,10 @@
 		},
 
 		addHooks: function () {
-			if (this._withins)
+			if (this._withins) {
 				this._withins.forEach(this._watch.bind(this, WITHIN));
+				this._featureGroup.on('layeradd layerremove', this._getHandler(this._validateMe, WITHIN));
+			}
 		},
 
 		fireOnMap: function (name, evt) {
@@ -27,7 +29,10 @@
 		},
 
 		removeHooks: function () {
-			this._unwithin();
+			if (this._withins) {
+				this._featureGroup.off('layeradd layerremove', this._getHandler(this._validateMe, WITHIN));
+				this._unwithin();
+			}
 		},
 
 		within: function () {
@@ -51,6 +56,10 @@
 		},
 
 		_validate: function (op, evt) {
+
+			if (!this._featureGroup.getLayers().length)
+				return;
+
 			var id = L.stamp(evt.target);
 
 			if (!this._featureGroup[op](evt.target)) {
@@ -61,7 +70,7 @@
 				if (this._errors[op].indexOf(id) === -1)
 					this._errors[op].push(id);
 
-				evt = {validation: op, targetLayer: this, sourceLayer: evt.target};
+				evt = {validation: op, targetLayer: this._featureGroup, sourceLayer: evt.target};
 
 				this.fire('invalid', evt);
 				this.fireOnMap('draw:invalid', evt);
@@ -72,12 +81,57 @@
 					if (index > -1) {
 						this._errors[op].splice(index, 1);
 
-						if (this._errors.length === 0) {
-							evt = {validation: op, targetLayer: this};
+						if (this._errors[op].length === 0) {
+							evt = {validation: op, targetLayer: this._featureGroup};
 							this.fire('valid', evt);
 							this.fireOnMap('draw:valid', evt);
 						}
 					}
+				}
+			}
+		},
+
+		_validateMe: function(op) {
+			var evt;
+			var valid = true;
+
+			if (this._errors[op] && this._errors[op].length)
+				valid = false;
+
+			this._errors[op] = [];
+
+			if (!this._featureGroup.getLayers().length) {
+				if (!valid) {
+					evt = {validation: op, targetLayer: this._featureGroup};
+					this.fire('valid', evt);
+					this.fireOnMap('draw:valid', evt);
+				}
+
+				return;
+			}
+
+			var layers = this['_' + op + 's'];
+
+			if (layers) {
+				evt = {validation: op, targetLayer: this._featureGroup};
+
+				
+				layers.forEach(function(layer) {
+
+					if (!this._featureGroup[op](layer)) {
+
+						this._errors[op].push(L.stamp(layer));
+						evt.sourceLayer = layer;
+						this.fire('invalid', evt);
+						this.fireOnMap('draw:invalid', evt);
+					}
+
+				}, this);
+
+				if (!this._errors[op].length && !valid) {
+					evt = {validation: op, targetLayer: this._featureGroup};
+					this.fire('valid', evt);
+					this.fireOnMap('draw:valid', evt);
 				}
 			}
 		},
@@ -92,8 +146,10 @@
 		},
 
 		_unwithin: function () {
-			if (this._withins)
+			if (this._withins) {
 				this._withins.forEach(this._unwatch.bind(this, WITHIN));
+				delete this._withins;
+			}
 		},
 
 		_unwatch: function (op, featureGroup) {
